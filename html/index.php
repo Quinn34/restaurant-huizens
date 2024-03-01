f3ec985e7b4f0fe5fb475d4c1c804f07d95d1a08
Quinn van Huizen <153827801+Quinn34@users.noreply.github.com>
2024-03-01 12:08:49 +0100

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" >
+    <script src="load.js"></script>
+</head>
+<header>
+    <div class="nav">
+        <div class="left-75">
+            <div class="name">
+                <p>Huizens Bakkerij</p>
+            </div>
+        </div>
+        <div class="right-25">
+            <div class="buttons">
+                <a>Home</a>
+            <a onclick="load('product.php')" class="Items">Producten</a>
+            <a onclick="load('contact.php')" class="Items">Contact</a>
+            <a onclick="load('login.php')" class="Items">Login</a>
+            </div>
+        </div>
+    </div>
+</header>
+<body>
+<div class="container1">
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+        </div>
+        <div class="container1">
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+        </div>
+        <div class="container1">
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+        </div>
+        <div class="container1">
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+            <div class="box1">
+                <img src="# class="imgF">
+            </div>
+        </div>
+</body>
+</html>
---
+++ html/pages/product.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" >
+    <script src="load.js"></script>
+</head>
+<body>
+    <div class="nav">
+        <div class="left-50">
+            <div class="name">
+                <p>Huizens Bakkerij</p>
+            </div>
+        </div>
+        <div class="right-50">
+            <div class="buttons">
+            <a onclick="load('index.php')" class="Items">Home</a>
+            <a href="#" target="_self">producten</a>
+            <a onclick="load('contact.php')" class="Items">Contact</a>
+            <a onclick="load('Login.php')" class="Items">Login</a>
+            </div>
+            </div>
+    </div>
+</body>
+</html>
